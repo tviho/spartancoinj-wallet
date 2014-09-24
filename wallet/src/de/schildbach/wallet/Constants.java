d1daf98f5c272d84cdab7090d0793f7c534ffd12
unknown <corefork@users.noreply.github.com>
2014-09-24 18:24:57 +0300

    main source code

---
+++ wallet/src/de/schildbach/wallet/Constants.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2011-2014 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.schildbach.wallet;
+
+import android.os.Build;
+import android.os.Environment;
+import android.text.format.DateUtils;
+
+import com.google.spartancoin.core.CoinParams;
+import com.google.spartancoin.core.NetworkParameters;
+import com.google.spartancoin.params.MainNetParams;
+import com.google.spartancoin.params.TestNet3Params;
+
+import java.io.File;
+
+/**
+ * @author Andreas Schildbach
+ */
+public final class Constants
+{
+	public static final boolean TEST = R.class.getPackage().getName().contains("_test");
+
+	/** Network this wallet is on (e.g. testnet or mainnet). */
+	public static final NetworkParameters NETWORK_PARAMETERS = TEST ? TestNet3Params.get() : MainNetParams.get();
+
+	public final static class Files
+	{
+		private static final String FILENAME_NETWORK_SUFFIX = NETWORK_PARAMETERS.getId().equals(NetworkParameters.ID_MAINNET) ? "" : "-testnet";
+
+		/** Filename of the wallet. */
+		public static final String WALLET_FILENAME_PROTOBUF = "wallet-protobuf" + FILENAME_NETWORK_SUFFIX;
+
+		/** Filename of the automatic key backup (old format, can only be read). */
+		public static final String WALLET_KEY_BACKUP_BASE58 = "key-backup-base58" + FILENAME_NETWORK_SUFFIX;
+
+		/** Filename of the automatic wallet backup. */
+		public static final String WALLET_KEY_BACKUP_PROTOBUF = "key-backup-protobuf" + FILENAME_NETWORK_SUFFIX;
+
+		/** Manual backups go here. */
+		public static final File EXTERNAL_WALLET_BACKUP_DIR = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+		/** Filename of the manual key backup (old format, can only be read). */
+		public static final String EXTERNAL_WALLET_KEY_BACKUP = CoinParams.coinName.toLowerCase() +"bitcoin-wallet-keys" + FILENAME_NETWORK_SUFFIX;
+
+		/** Filename of the manual wallet backup. */
+		public static final String EXTERNAL_WALLET_BACKUP = CoinParams.coinName.toLowerCase() +"bitcoin-wallet-backup" + FILENAME_NETWORK_SUFFIX;
+
+		/** Filename of the block store for storing the chain. */
+		public static final String BLOCKCHAIN_FILENAME = "blockchain" + FILENAME_NETWORK_SUFFIX;
+
+		/** Filename of the block checkpoints file. */
+		public static final String CHECKPOINTS_FILENAME = "checkpoints" + FILENAME_NETWORK_SUFFIX;
+	}
+
+	/** Maximum size of backups. Files larger will be rejected. */
+	public static final long BACKUP_MAX_CHARS = 5000000;
+
+	private static final String EXPLORE_BASE_URL_PROD = CoinParams.BLOCKEXPLORER_BASE_URL_PROD;
+	private static final String EXPLORE_BASE_URL_TEST = CoinParams.BLOCKEXPLORER_BASE_URL_TEST;
+	/** Base URL for browsing transactions, blocks or addresses. */
+	public static final String EXPLORE_BASE_URL = NETWORK_PARAMETERS.getId().equals(NetworkParameters.ID_MAINNET) ? EXPLORE_BASE_URL_PROD
+			: EXPLORE_BASE_URL_TEST;
+
+	private static final String BITEASY_API_URL_PROD = CoinParams.UNSPENT_API_URL;
+	private static final String BITEASY_API_URL_TEST = CoinParams.UNSPENT_API_URL_TEST;
+	/** Base URL for blockchain API. */
+	public static final String BITEASY_API_URL = NETWORK_PARAMETERS.getId().equals(NetworkParameters.ID_MAINNET) ? BITEASY_API_URL_PROD
+			: BITEASY_API_URL_TEST;
+
+	/** URL to fetch version alerts from. */
+	public static final String VERSION_URL = "http://wallet.schildbach.de/version";
+
+	/** MIME type used for transmitting single transactions. */
+	public static final String MIMETYPE_TRANSACTION = "application/x-"+CoinParams.coinName.toLowerCase()+"tx";
+
+	/** MIME type used for transmitting wallet backups. */
+	public static final String MIMETYPE_WALLET_BACKUP = "application/x-"+CoinParams.coinName.toLowerCase()+"-wallet-backup";
+
+	/** Number of confirmations until a transaction is fully confirmed. */
+	public static final int MAX_NUM_CONFIRMATIONS = 7;
+
+	/** User-agent to use for network access. */
+	public static final String USER_AGENT = CoinParams.coinName+" Wallet";
+
+	/** Default currency to use if all default mechanisms fail. */
+	public static final String DEFAULT_EXCHANGE_CURRENCY = "USD";
+
+	/** Currency code for base 1 SpartanCoin. */
+	public static final String CURRENCY_CODE_BTC = CoinParams.coinTicker;
+
+	/** Currency code for base 1/1000 SpartanCoin. */
+	public static final String CURRENCY_CODE_MBTC = "m"+CoinParams.coinTicker;
+
+	/** Currency code for base 1/1000000 SpartanCoin. */
+	public static final String CURRENCY_CODE_UBTC = "µ"+CoinParams.coinTicker;
+
+	/** Donation address for tip/donate action. */
+	public static final String DONATION_ADDRESS = CoinParams.DONATION_ADDRESS;
+
+	/** Recipient e-mail address for reports. */
+	public static final String REPORT_EMAIL = "issues@spartancoin.org";
+
+	/** Subject line for manually reported issues. */
+	public static final String REPORT_SUBJECT_ISSUE = "Reported issue";
+
+	/** Subject line for crash reports. */
+	public static final String REPORT_SUBJECT_CRASH = "Crash report";
+
+	public static final char CHAR_HAIR_SPACE = '\u200a';
+	public static final char CHAR_THIN_SPACE = '\u2009';
+	public static final char CHAR_ALMOST_EQUAL_TO = '\u2248';
+	public static final char CHAR_CHECKMARK = '\u2713';
+	public static final String CURRENCY_PLUS_SIGN = "+" + CHAR_THIN_SPACE;
+	public static final String CURRENCY_MINUS_SIGN = "-" + CHAR_THIN_SPACE;
+	public static final String PREFIX_ALMOST_EQUAL_TO = Character.toString(CHAR_ALMOST_EQUAL_TO) + CHAR_THIN_SPACE;
+	public static final int ADDRESS_FORMAT_GROUP_SIZE = 4;
+	public static final int ADDRESS_FORMAT_LINE_SIZE = 12;
+
+	public static final int BTC_MAX_PRECISION = 8;
+	public static final int MBTC_MAX_PRECISION = 5;
+	public static final int UBTC_MAX_PRECISION = 2;
+	public static final int LOCAL_PRECISION = 8;
+
+	public static final String LICENSE_URL = "http://www.gnu.org/licenses/gpl-3.0.txt";
+	public static final String SOURCE_URL = "https://github.com/spartanncoin/spartancoin-android-wallet";
+	public static final String BINARY_URL = "https://github.com/spartanncoin/spartancoin-android-wallet/releases";
+	public static final String CREDITS_BITCOINJ_URL = "https://github.com/spartanncoin/spartancoinj/";
+	public static final String CREDITS_ZXING_URL = "https://github.com/zxing/zxing";
+	public static final String CREDITS_ICON_URL = "https://bitcointalk.org/index.php?action=profile;u=238074";
+	public static final String AUTHOR_TWITTER_URL = "https://twitter.com/spartancoin";
+	public static final String AUTHOR_GOOGLEPLUS_URL = "https://profiles.google.com/andreas.schildbach";
+	public static final String COMMUNITY_GOOGLEPLUS_URL = "https://plus.google.com/103618764540319577065/posts/EneHZbq35Eq";
+	public static final String MARKET_APP_URL = "market://details?id=%s";
+	public static final String WEBMARKET_APP_URL = "";//"https://play.google.com/store/apps/details?id=%s";
+	public static final String MARKET_PUBLISHER_URL = "market://search?q=pub:"+CoinParams.coinName;
+
+	public static final int HTTP_TIMEOUT_MS = 15 * (int) DateUtils.SECOND_IN_MILLIS;
+
+	public static final long LAST_USAGE_THRESHOLD_JUST_MS = DateUtils.HOUR_IN_MILLIS;
+	public static final long LAST_USAGE_THRESHOLD_RECENTLY_MS = 2 * DateUtils.DAY_IN_MILLIS;
+
+	public static final int SDK_JELLY_BEAN = 16;
+	public static final int SDK_JELLY_BEAN_MR2 = 18;
+
+	public static final int SDK_DEPRECATED_BELOW = Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+
+	public static final boolean BUG_OPENSSL_HEARTBLEED = Build.VERSION.SDK_INT == Constants.SDK_JELLY_BEAN
+			&& Build.VERSION.RELEASE.startsWith("4.1.1");
+
+	public static final int MEMORY_CLASS_LOWEND = 48;
+}
---
+++ wallet/src/de/schildbach/wallet/WalletBalanceWidgetProvider.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2011-2014 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.schildbach.wallet;
+
+import java.math.BigInteger;
+
+import javax.annotation.Nonnull;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.preference.PreferenceManager;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.view.View;
+import android.widget.RemoteViews;
+
+import com.google.spartancoin.core.Wallet;
+import com.google.spartancoin.core.Wallet.BalanceType;
+
+import de.schildbach.wallet.ExchangeRatesProvider.ExchangeRate;
+import de.schildbach.wallet.ui.RequestCoinsActivity;
+import de.schildbach.wallet.ui.SendCoinsQrActivity;
+import de.schildbach.wallet.ui.WalletActivity;
+import de.schildbach.wallet.ui.send.SendCoinsActivity;
+import de.schildbach.wallet.util.GenericUtils;
+import de.schildbach.wallet.util.WalletUtils;
+import de.schildbach.wallet.R;
+
+/**
+ * @author Andreas Schildbach
+ */
+public class WalletBalanceWidgetProvider extends AppWidgetProvider
+{
+	private static final Logger log = LoggerFactory.getLogger(WalletBalanceWidgetProvider.class);
+
+	@Override
+	public void onUpdate(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds)
+	{
+		final WalletApplication application = (WalletApplication) context.getApplicationContext();
+		final Wallet wallet = application.getWallet();
+		final BigInteger balance = wallet.getBalance(BalanceType.ESTIMATED);
+
+		updateWidgets(context, appWidgetManager, appWidgetIds, balance);
+	}
+
+	public static void updateWidgets(final Context context, final Wallet wallet)
+	{
+		final AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+
+		final ComponentName providerName = new ComponentName(context, WalletBalanceWidgetProvider.class);
+
+		try
+		{
+			final int[] appWidgetIds = appWidgetManager.getAppWidgetIds(providerName);
+
+			if (appWidgetIds.length > 0)
+			{
+				final BigInteger balance = wallet.getBalance(BalanceType.ESTIMATED);
+
+				WalletBalanceWidgetProvider.updateWidgets(context, appWidgetManager, appWidgetIds, balance);
+			}
+		}
+		catch (final RuntimeException x) // system server dead?
+		{
+			log.warn("cannot update app widgets", x);
+		}
+	}
+
+	private static void updateWidgets(final Context context, @Nonnull final AppWidgetManager appWidgetManager, @Nonnull final int[] appWidgetIds,
+			@Nonnull final BigInteger balance)
+	{
+		final Configuration config = new Configuration(PreferenceManager.getDefaultSharedPreferences(context));
+
+		final Cursor data = context.getContentResolver().query(ExchangeRatesProvider.contentUri(context.getPackageName(), true), null,
+				ExchangeRatesProvider.KEY_CURRENCY_CODE, new String[] { config.getExchangeCurrencyCode() }, null);
+		final SpannableStringBuilder localBalanceStr;
+		if (data != null)
+		{
+			if (data.moveToFirst())
+			{
+				final ExchangeRate exchangeRate = ExchangeRatesProvider.getExchangeRate(data);
+				final BigInteger localBalance = WalletUtils.localValue(balance, exchangeRate.rate);
+				localBalanceStr = new SpannableStringBuilder(GenericUtils.formatValue(localBalance, Constants.LOCAL_PRECISION, 0));
+				WalletUtils.formatSignificant(localBalanceStr, new RelativeSizeSpan(0.85f));
+				final String prefix = Constants.PREFIX_ALMOST_EQUAL_TO + GenericUtils.currencySymbol(exchangeRate.currencyCode)
+						+ Constants.CHAR_THIN_SPACE;
+				localBalanceStr.insert(0, prefix);
+				localBalanceStr.setSpan(new RelativeSizeSpan(0.85f), 0, prefix.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+				localBalanceStr.setSpan(new ForegroundColorSpan(context.getResources().getColor(R.color.fg_less_significant)), 0, prefix.length(),
+						Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+			}
+			else
+			{
+				localBalanceStr = null;
+			}
+
+			data.close();
+		}
+		else
+		{
+			localBalanceStr = null;
+		}
+
+		final Spannable balanceStr = new SpannableString(GenericUtils.formatValue(balance, config.getBtcPrecision(), config.getBtcShift()));
+		WalletUtils.formatSignificant(balanceStr, WalletUtils.SMALLER_SPAN);
+
+		for (final int appWidgetId : appWidgetIds)
+		{
+			final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.wallet_balance_widget_content);
+
+			final String currencyCode = config.getBtcPrefix();
+			if (Constants.CURRENCY_CODE_BTC.equals(currencyCode))
+				views.setImageViewResource(R.id.widget_wallet_prefix, R.drawable.currency_symbol_btc);
+			else if (Constants.CURRENCY_CODE_MBTC.equals(currencyCode))
+				views.setImageViewResource(R.id.widget_wallet_prefix, R.drawable.currency_symbol_mbtc);
+			else if (Constants.CURRENCY_CODE_UBTC.equals(currencyCode))
+				views.setImageViewResource(R.id.widget_wallet_prefix, R.drawable.currency_symbol_ubtc);
+
+			views.setTextViewText(R.id.widget_wallet_balance_btc, balanceStr);
+			views.setViewVisibility(R.id.widget_wallet_balance_local, localBalanceStr != null ? View.VISIBLE : View.GONE);
+			views.setTextViewText(R.id.widget_wallet_balance_local, localBalanceStr);
+
+			views.setOnClickPendingIntent(R.id.widget_button_balance,
+					PendingIntent.getActivity(context, 0, new Intent(context, WalletActivity.class), 0));
+			views.setOnClickPendingIntent(R.id.widget_button_request,
+					PendingIntent.getActivity(context, 0, new Intent(context, RequestCoinsActivity.class), 0));
+			views.setOnClickPendingIntent(R.id.widget_button_send,
+					PendingIntent.getActivity(context, 0, new Intent(context, SendCoinsActivity.class), 0));
+			views.setOnClickPendingIntent(R.id.widget_button_send_qr,
+					PendingIntent.getActivity(context, 0, new Intent(context, SendCoinsQrActivity.class), 0));
+
+			appWidgetManager.updateAppWidget(appWidgetId, views);
+		}
+	}
+}
